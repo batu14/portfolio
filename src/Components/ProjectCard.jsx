7e11586969143f2e11caaef54b264b8554a9872a
batu14 <batuhanciftci2000@hotmail.com>
2025-07-21 02:05:06 +0300

    Tasarım

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -12,9 +12,9 @@ const ProjectCard = ({ item, index }) => {
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
-      className="bg-white group aspect-video scroll-image flex items-center justify-center w-full h-full rounded-lg shadow-sm border border-gray-100 hover:shadow-md transition-shadow duration-300 relative"
+      className="bg-white overflow-hidden group aspect-video scroll-image flex items-center justify-center w-full h-full rounded-lg shadow-sm border border-gray-100 hover:shadow-md transition-shadow duration-300 relative"
     >
-      <div className="w-full flex items-center p-6 overflow-hidden lg:justify-between justify-end absolute md:group-hover:h-1/5 h-1/5 bottom-0 lg:bg-black/60 bg-black/0 md:group-hover:z-10 z-10 md:group-hover:opacity-100 opacity-100 md:group-hover:visible visible transition-all duration-300 backdrop-blur-sm md:h-0 md:opacity-0 md:invisible">
+      <div className="w-full flex items-center p-6 overflow-hidden lg:justify-between justify-end absolute md:group-hover:h-1/5 h-1/5 bottom-0 rounded-b-lg lg:bg-black/60 bg-black/0 md:group-hover:z-10 z-10 md:group-hover:opacity-100 opacity-100 md:group-hover:visible visible transition-all duration-300 backdrop-blur-sm md:h-0 md:opacity-0 md:invisible">
         <div className=" hidden md:flex items-center gap-2 text-white/80 text-sm font-medium">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
